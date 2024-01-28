3e0ff1e3cf8d5c8a34f32808dab4d7656f20cc28
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-01-28 15:24:21 +0100

    Add empty Laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Notes App</title>
+
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="bg-gray-500">
+
+    </body>
+</html>

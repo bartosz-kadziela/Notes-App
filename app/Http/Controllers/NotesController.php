96ddb47626f8686ba44f34c654799076dd7ef654
Bartosz Kądziela <bartosz.kadziela@o2.pl>
2024-02-03 17:56:14 +0100

    Add note model and logic for storing notes in database

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class NotesController extends Controller
 {
     public function store(Request $request)
     {
-        dd($request->all()); // dd = die and dump
+        Note::create([
+            'title' => $request->get('title'),
+            'body' => $request->get('body'),
+        ]);
+
+        return redirect()->back();
     }
 }
